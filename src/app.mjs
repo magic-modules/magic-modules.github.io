87a72260f0ee81d084d067ec2f3a889bcf15a80d
Jascha <jascha@jaeh.at>
2019-05-20 12:38:16 +0200

    update docs, test, gitignore

--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,7 @@
-module.exports = {
-  state: {
+export const state = {
     title: '@magic-modules',
     description: '@magic-modules. building blocks for your @magic app.',
     logotext: '@magic-modules',
     logo: '/logo.png',
-    menu: [
-      { to: '/#gl-magic-modules', text: 'magic-modules' },
-    ],
-  },
-}
+    menu: [{ to: '/#gl-magic-modules', text: 'magic-modules' }],
+  }
--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -1,4 +1,4 @@
-module.exports = () => [
+export const View = () => [
   h2(Link({ to: 'https://magic.github.io' }, '@magic')),
   p('magic is a module/component based generator for static pages and serverless deployments.'),
   p(
@@ -8,5 +8,5 @@ module.exports = () => [
 
   h2({ id: 'magic-modules' }, Link({ to: 'https://github.com/magic-modules/' }, '@magic-modules')),
   p('modules are the building blocks of @magic.'),
-  ModuleList,
+  ModuleList(),
 ]
