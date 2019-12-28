ec1477f4844b7b5d34e828441dcfb19bb699ab35
Jascha <jascha@jaeh.at>
2019-12-28 18:11:32 +0100

    alphabetize menu by magic-module names

--- src/app.mjs
+++ src/app.mjs
@@ -3,5 +3,25 @@ export const state = {
   description: '@magic-modules. building blocks for your @magic app.',
   logotext: '@magic-modules',
   logo: '/logo.png',
-  menu: [{ to: '/#gl-magic-modules', text: 'magic-modules' }],
+  menu: [
+    {
+      to: '/#gl-magic-modules',
+      text: 'magic-modules',
+      items: [
+        { to: '#gl-magic-modules-li-accordion', text: 'accordion' },
+        { to: '#gl-magic-modules-li-gdpr', text: 'gdpr' },
+        { to: '#gl-magic-modules-li-git-badges', text: 'git-badges' },
+        { to: '#gl-magic-modules-li-git-list', text: 'git-list' },
+        { to: '#gl-magic-modules-li-hero', text: 'hero' },
+        { to: '#gl-magic-modules-li-language-switch', text: 'language-switch' },
+        { to: '#gl-magic-modules-li-library-list', text: 'library-list' },
+        { to: '#gl-magic-modules-li-messages', text: 'messages' },
+        { to: '#gl-magic-modules-li-module-list', text: 'module-list' },
+        { to: '#gl-magic-modules-li-pre', text: 'pre' },
+        { to: '#gl-magic-modules-li-sound-cloud', text: 'sound-cloud' },
+        { to: '#gl-magic-modules-li-theme-list', text: 'theme-list' },
+        { to: '#gl-magic-modules-li-video-embed', text: 'video-embed' },
+      ],
+    },
+  ],
 }
