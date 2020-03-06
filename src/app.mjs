489e28781c807bfa98bb2442891043472c9944ca
jascha <jascha@jaeh.at>
2020-03-06 05:43:24 +0100

    app: add meta data for project theme

--- src/app.mjs
+++ src/app.mjs
@@ -1,30 +1,38 @@
 export const state = {
   title: '@magic-modules',
   description: '@magic-modules. building blocks for your @magic app.',
-  logotext: '@magic-modules',
-  logo: '/logo.png',
+
   menu: [
-    {
-      to: '/#gl-magic-modules',
-      text: 'magic-modules',
-      items: [
-        { to: '-li-accordion', text: 'accordion' },
-        { to: '-li-gdpr', text: 'gdpr' },
-        { to: '-li-git-badges', text: 'git-badges' },
-        { to: '-li-git-list', text: 'git-list' },
-        { to: '-li-hero', text: 'hero' },
-        { to: '-li-language-switch', text: 'language-switch' },
-        { to: '-li-library-list', text: 'library-list' },
-        { to: '-li-light-switch', text: 'light-switch' },
-        { to: '-li-messages', text: 'messages' },
-        { to: '-li-module-list', text: 'module-list' },
-        { to: '-li-pre', text: 'pre' },
-        { to: '-li-sound-cloud', text: 'sound-cloud' },
-        { to: '-li-theme-list', text: 'theme-list' },
-        { to: '-li-theme-vars', text: 'theme-vars' },
-        { to: '-li-video-embed', text: 'video-embed' },
-        { to: '-li-xkcd', text: 'xkcd embed' },
+    { to: '/', text: 'about' },
+    { to: '/#gl-magic-modules', text: 'modules' },
+  ],
+
+  hero: {
+    title: 'modules',
+    description: 'building blocks for your @magic app.',
+  },
+
+  footer: {
+    one: {
+      title: '@magic',
+      before: ['modules', 'created by', 'Wizards & Witches'],
+    },
+
+    two: {
+      title: 'projects',
+      menu: [
+        { to: 'https://github.com/magic/', text: '@magic' },
+        { to: 'https://github.com/grundstein/', text: '@grundstein' },
+        { to: 'https://github.com/webboot/', text: '@webboot' },
       ],
     },
-  ],
+
+    three: {
+      title: 'social',
+      menu: [
+        { to: 'https://npmjs.com/org/magic-modules/', text: 'npm' },
+        { to: 'https://github.com/magic-modules/', text: 'github' },
+      ],
+    },
+  },
 }
